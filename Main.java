ab742306da004476528f3d5e212350251b13842d
arcadijobs <arcadijobs@gmail.com>
2020-03-11 15:05:27 +0300

    messege

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-      int a = 10;
-      int c = 11;
-      int b = 11;
-      int d = 11;  
+//      int a = 10;
+//      int c = 11;
+//      int b = 11;
+//      int d = 11;  
         System.out.println(a + c + b + d);
     }
 }
