575d7741a7f5e2043272ffa399578249708c22f0
arcadijobs <arcadijobs@gmail.com>
2020-03-11 09:34:43 +0300

    second commit

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
       int a = 10;
       int c = 11;
-        System.out.println(a + c);
+      int b = 11;
+      int d = 11;  
+        System.out.println(a + c + b + d);
     }
 }
