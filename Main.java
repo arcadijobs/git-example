c60a3021b03d21f5e04797d0f22f9faef84db390
arcadijobs <arcadijobs@gmail.com>
2020-03-11 09:23:14 +0300

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+      int a = 10;
+      int c = 11;
+        System.out.println(a + c);
+    }
+}
---
+++ Main2.java
@@ -0,0 +1,7 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int b = 10;
+        int c = 12;
+        System.out.println(b + c);
+    }
+}
